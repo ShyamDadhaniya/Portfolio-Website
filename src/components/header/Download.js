7fe38736d44b6f80dbae62d301b23490ef4ad2a8
SHYAM DADHANIYA <74183113+ShyamDadhaniya@users.noreply.github.com>
2022-05-24 01:14:56 +0530

    Update

---
+++ src/components/header/Download.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+const Download = () => {
+  return (
+    <>
+    <Button variant="primary" href="" className="btn-download">Download</Button>
+    <Button variant="primary" href="" className="btn-download">Let's Talk</Button>
+    </>
+  )
+}
+
+export default Download
---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Download from '../header/Download'
+
+const Header = () => {
+  return (
+<>
+<h5>Hello I'm</h5>
+<h1>Shyam Dadhaniya</h1>
+<h5>React Developer</h5>
+<Download/>
+</>  )
+}
+
+export default Header
